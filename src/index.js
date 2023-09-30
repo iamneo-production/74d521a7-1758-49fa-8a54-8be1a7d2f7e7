5982bec9f123ad546285c28748896026bec531ed
Alsa5 <alsa21104.IT@rmkec.ac.in>
2023-09-30 04:46:59 +0000

    commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Srcipt';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <HashRouter> */}
+    <App />
+    {/* </HashRouter> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
